f33b89c731778544f288a9302efe77731d1fdd46
Prokhor <gerlynd.prokhor@gmail.com>
2021-09-10 09:52:02 +0300

    vtoroy commit as Proshayrus

---
+++ Work_with_git_lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_with_git_lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I\'m Proshayrus!");
+            Console.ReadKey();
+        }
+    }
+}
